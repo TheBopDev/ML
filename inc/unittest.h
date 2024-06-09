8006ba39b74ffdc937acd9b475d978cc865f25ad
TheBopDev <josh@bop.dev>
2024-06-09 13:47:08 -0500

    Added unit test for tanh

--- inc/unittest.h
+++ inc/unittest.h
@@ -12,6 +12,7 @@ uint16_t fnu16_test_afn_all(void);
 uint8_t fnu8_test_afn_mean(double *pad_input, int i_array_length);
 uint8_t fnu8_test_afn_sigmoid(double d_input);
 uint8_t fnu8_test_afn_identity(double d_input);
+uint8_t fnu8_test_afn_tanh(double d_input);
 
 
 
